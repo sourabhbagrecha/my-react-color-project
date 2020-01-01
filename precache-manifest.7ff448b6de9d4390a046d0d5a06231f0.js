babba45986ee39398de3037969066daea2d5b97f
Sourabh Bagrecha <sourabhbagrecha@gmail.com>
2020-01-01 13:04:48 +0530

    Updates

---
+++ precache-manifest.7ff448b6de9d4390a046d0d5a06231f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38bb4d6dc1a349843cbaab11a4cd9ce5",
+    "url": "/my-react-color-project/index.html"
+  },
+  {
+    "revision": "33442b62e092bcf00693",
+    "url": "/my-react-color-project/static/css/2.753ea2eb.chunk.css"
+  },
+  {
+    "revision": "5a35f66993a7270be67b",
+    "url": "/my-react-color-project/static/css/main.94f8948d.chunk.css"
+  },
+  {
+    "revision": "33442b62e092bcf00693",
+    "url": "/my-react-color-project/static/js/2.f32a2dc6.chunk.js"
+  },
+  {
+    "revision": "42c3c94a3e26be691c31b5d2b0c63158",
+    "url": "/my-react-color-project/static/js/2.f32a2dc6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a35f66993a7270be67b",
+    "url": "/my-react-color-project/static/js/main.1caeb108.chunk.js"
+  },
+  {
+    "revision": "4bde082d68bb7ce4a140",
+    "url": "/my-react-color-project/static/js/runtime-main.ba5a5f3d.js"
+  }
+]);
